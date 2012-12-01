80a5895652fdd0d0d477e56d762cfb2fc4f43c96
Femi Veys <femi@tribeforce.com>
2012-12-01 10:02:06 -0500

    Toolbar hide fix to dev version

--- sites/all/modules/contrib/toolbar_hide/toolbar_hide.js
+++ sites/all/modules/contrib/toolbar_hide/toolbar_hide.js
@@ -1,14 +1,32 @@
-// $Id: toolbar_hide.js,v 1.1.2.4 2010/11/13 21:30:07 joshthegeek Exp $
-
 (function($){
-if (Drupal.settings.toolbar_hide.default) {
-  $(document).ready(function(){$('#toolbar').hide();$('body').addClass('tbr_menu_hidden');});
+if (Drupal.settings.toolbar_hide.onload) {
+  $(document).ready(
+    function() {
+      $('#toolbar').hide();
+      $('body').addClass('tbr_menu_hidden');
+  });
 }
+$(document.ready(
+  function() {
+    if ($.cookie('toolbar_hide_hidden')) {
+      $('#toolbar').hide();
+      $('body').addClass('tbr_menu_hidden');
+    } else {
+      $('#toolbar').show();
+      $('body').removeClass('tbr_menu_hidden');
+    }
+  }
+);
 $(document).keypress(function(e) {
-	var unicode=e.keyCode? e.keyCode : e.charCode;
-	if (String.fromCharCode(unicode)==Drupal.settings.toolbar_hide.key){
-	  $('#toolbar').slideToggle('fast');
-	  $('body').toggleClass('tbr_menu_hidden');
-	}
+  var unicode=e.keyCode? e.keyCode : e.charCode;
+  if (String.fromCharCode(unicode)==Drupal.settings.toolbar_hide.key){
+    $('#toolbar').slideToggle('fast');
+    $('body').toggleClass('tbr_menu_hidden');
+    if ($('body').hasClass('tbr_hide_hidden')) {
+      $.cookie('toolbar_hide_hidden', 1);
+    } else {
+      $.cookie('toolbar_hide_hidden, 0);
+    }
+  }
 });
 })(jQuery);
