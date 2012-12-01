05e638bbc7f696598d7eea51d68c5aedc2d50741
Femi Veys <femi@tribeforce.com>
2012-12-01 10:13:29 -0500

    Toolbar hide fix dev version in JS

--- sites/all/modules/contrib/toolbar_hide/toolbar_hide.js
+++ sites/all/modules/contrib/toolbar_hide/toolbar_hide.js
@@ -1,32 +1,32 @@
 (function($){
-if (Drupal.settings.toolbar_hide.onload) {
-  $(document).ready(
+  if (Drupal.settings.toolbar_hide.onload) {
+    $(document).ready(
+      function() {
+        $('#toolbar').hide();
+        $('body').addClass('tbr_menu_hidden');
+    });
+  }
+  $(document.ready(
     function() {
-      $('#toolbar').hide();
-      $('body').addClass('tbr_menu_hidden');
+      if ($.cookie('toolbar_hide_hidden')) {
+        $('#toolbar').hide();
+        $('body').addClass('tbr_menu_hidden');
+      } else {
+        $('#toolbar').show();
+        $('body').removeClass('tbr_menu_hidden');
+      }
+    }
+  ));
+  $(document).keypress(function(e) {
+    var unicode=e.keyCode? e.keyCode : e.charCode;
+    if (String.fromCharCode(unicode)==Drupal.settings.toolbar_hide.key){
+      $('#toolbar').slideToggle('fast');
+      $('body').toggleClass('tbr_menu_hidden');
+      if ($('body').hasClass('tbr_hide_hidden')) {
+        $.cookie('toolbar_hide_hidden', 1);
+      } else {
+        $.cookie('toolbar_hide_hidden', 0);
+      }
+    }
   });
-}
-$(document.ready(
-  function() {
-    if ($.cookie('toolbar_hide_hidden')) {
-      $('#toolbar').hide();
-      $('body').addClass('tbr_menu_hidden');
-    } else {
-      $('#toolbar').show();
-      $('body').removeClass('tbr_menu_hidden');
-    }
-  }
-);
-$(document).keypress(function(e) {
-  var unicode=e.keyCode? e.keyCode : e.charCode;
-  if (String.fromCharCode(unicode)==Drupal.settings.toolbar_hide.key){
-    $('#toolbar').slideToggle('fast');
-    $('body').toggleClass('tbr_menu_hidden');
-    if ($('body').hasClass('tbr_hide_hidden')) {
-      $.cookie('toolbar_hide_hidden', 1);
-    } else {
-      $.cookie('toolbar_hide_hidden, 0);
-    }
-  }
-});
 })(jQuery);
